0c1e109800fe91e8506b3bf45f71a0676134819d
JulyBelova <july@belova.es>
2024-04-04 22:33:25 +0300

    delete storyboard

---
+++ Rick-Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick-Morty
+//
+//  Created by July Belova on 04.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+
+
+}
+
